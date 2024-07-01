3ee589aa0d3ca88cdd907337f326caf8dda09db7
Dhananjay23-17 <ui23ec17@iiitsurat.ac.in>
2024-07-01 22:30:22 +0530

    Add files via upload

---
+++ splash.js
@@ -0,0 +1,18 @@
+let logo=document.querySelector('.looggo');
+
+
+window.addEventListener('DOMContentLoaded',()=>{
+
+
+    setTimeout(()=>{
+        
+        setTimeout(()=>{
+
+            logo.style.top='-100vh';
+
+        }, 1500)
+
+    
+    })
+
+})
